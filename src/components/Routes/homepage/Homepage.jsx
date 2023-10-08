5adbda7619d26fb8195e5efb99e7c356015dcff9
moravega <victor@moravega.cr>
2023-10-08 17:30:02 -0600

    Adding loading animation

--- src/components/Routes/homepage/Homepage.jsx
+++ src/components/Routes/homepage/Homepage.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Globe from './components/Globe'
+import LoadingPage from '../../LoadingPage'
 
 const Homepage = () => {
+  const [loading, setLoading] = useState(true)
+  
+  function globeIsLoading(isLoading) {
+    setLoading(isLoading)
+  }
+
+
   return (
-    <div className = 'homeContainer'>
-        <Globe />
-    </div>
+    <>
+      <LoadingPage showLoadingPage={loading}/>
+      <div className = 'homeContainer'>
+        <Globe globeIsLoading={globeIsLoading}/>
+      </div>
+    </>
   )
 }
 
