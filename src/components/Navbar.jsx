2ded654ef65da26b190fe74c16b366290a7a2674
theorangeoutdoors <carolinams0301@gmail.com>
2023-10-08 14:14:03 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,52 +16,26 @@ const Navbar = () => {
 
   return (
     <div className='navbarContainer'>
-       <div className={isOpen ? 'menu':'menu hidden'}>
-            <div className='content'>
-                <div className='linksContainer'>
-                    <Link className='button' to='/'>
-                        <p className={location.pathname === '/' ? 'link active':'link'} >Home</p>
-                    </Link>
-                    <Link className='button' to='/about'>
-                        <p className={location.pathname === '/about' ? 'link active':'link'}>About us</p>
-                    </Link>
-                    {location.pathname === '/about' || location.pathname === '/about/our-team' || location.pathname === '/about/space-apps' ? 
-                        <Link className='button' to='about/our-team'>
-                            <p className={location.pathname === '/about/our-team' ? 'active indent': 'indent'}>Our Team</p>
-                        </Link>:''
-                    }
-                    {location.pathname === '/about' || location.pathname === '/about/our-team' || location.pathname === '/about/space-apps' ? 
-                        <Link className='button' to='about/space-apps'>
-                            <p className={location.pathname === '/about/space-apps' ? 'active indent': 'indent'}>Space Apps</p>
-                        </Link>:''
-                    }
-                    <Link className='button' to='/moon'>
-                        <p className={location.pathname === '/moon' ? 'link active':'link'}>Moon</p>
-                    </Link>
-                    {location.pathname === '/moon' || location.pathname === '/moon/missions'? 
-                        <Link className='button' to='/moon/missions'>
-                            <p className={location.pathname === '/moon/missions' ? 'active indent': 'indent'}>Missions</p>
-                        </Link>:''
-                    }
-                    <Link className='button' to='/quakes'>
-                        <p className={location.pathname === '/quakes' ? 'link active':'link'}>Quakes</p>
-                    </Link>
-                    <Link className='button' to='/404'>
-                        <p className={location.pathname === '/500' ? 'link active':'link'}>Future ...</p>
-                    </Link>
-                    <Link className='button' to='/resources'>
-                        <p className={location.pathname === '/resources' ? 'link active':'link'}>Resources</p>
-                    </Link>
-                </div>
-            </div>
+       <div className='menu'>
+            <Link className='button' to='/'>
+                <p className={location.pathname === '/' ? 'link active':'link'} >Home</p>
+            </Link>
+            <Link className='button' to='/about'>
+                <p className={location.pathname === '/about' ? 'link active':'link'}>About us</p>
+            </Link>
+            <Link className='button' to='/moon'>
+                <p className={location.pathname === '/moon' ? 'link active':'link'}>Moon</p>
+            </Link>
+            <Link className='button' to='/quakes'>
+                <p className={location.pathname === '/quakes' ? 'link active':'link'}>Quakes</p>
+            </Link>
+            <Link className='button' to='/404'>
+                <p className={location.pathname === '/500' ? 'link active':'link'}>Future ...</p>
+            </Link>
+            <Link className='button' to='/resources'>
+                <p className={location.pathname === '/resources' ? 'link active':'link'}>Resources</p>
+            </Link>
         </div>
-        <button className='toggler' onClick = {openNavbar}>
-            {!isOpen ? 
-                <i class="bi bi-list" style={{fontSize: '2.5vmax', color: 'white'}}></i>
-                :
-                <i class="bi bi-x-lg" style={{fontSize: '2.5vmax', color: 'white'}}></i>
-            }
-        </button>
     </div>
   )
 }
