6bf40dddf177e6ffba2e33cf1e77915a4d7d8147
theorangeoutdoors <carolinams0301@gmail.com>
2023-10-08 14:15:20 -0600

    Update Moon.jsx

--- src/components/Routes/moon/Moon.jsx
+++ src/components/Routes/moon/Moon.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import EmptyGlobe from '../../EmptyGlobe'
 
 const Moon = () => {
   return (
     <div className='moonInfoContainer'>
         <div className='information'>
-            <p className='subtitle'>
-              HOLA
-            </p>
+          <p className='subtitle'>
+            <div className='block'>
+              <p className='title'>Moon</p>
+              <p>View more</p>
+            </div>
+          </p>
+          <Link className='link' to='missions'>
+              <div className='button'>
+                <p className='subtitle'>Vizualize lunar missions</p>
+              </div>
+          </Link>
         </div>
         <EmptyGlobe />
     </div>
